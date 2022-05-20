84dcb4b7bc2de5bc362937b145ba0ca189fad138
lukasze <wyspianski.lukasz@gmail.com>
2022-05-20 14:55:05 +0200

    Initialise project

---
+++ src/main/java/com/example/gitplayground/GitPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitplayground/GitPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
